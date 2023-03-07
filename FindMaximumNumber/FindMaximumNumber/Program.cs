93b4080bbba2f4849793b47984a6c95648636b6c
Prachi <prachikale136@gmail.com>
2023-03-07 23:24:56 +0530

    [Prachi] Add: find maximun number among in three number

---
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMaximumNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int check = CheckMaxNumber.MaximumNumber(10, 20, 30);
+            Console.WriteLine(check);
+        }
+    }
+}
