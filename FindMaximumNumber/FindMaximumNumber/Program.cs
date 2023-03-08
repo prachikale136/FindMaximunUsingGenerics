5ac7208ba97fae96fbf4469bc22c726260bfc04e
Prachi <prachikale136@gmail.com>
2023-03-08 16:20:02 +0530

    [Prachi] Add: Update code for find maximun number

--- FindMaximumNumber/FindMaximumNumber/Program.cs
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
             int check = CheckMaxNumber.MaximumNumber(10, 20, 30);
+            int check1 = CheckMaxNumber.MaximumNumber(30, 20, 10);
+            int check2 = CheckMaxNumber.MaximumNumber(20,30, 10);
             Console.WriteLine(check);
+            Console.WriteLine(check1);
+            Console.WriteLine(check2);
         }
     }
 }
