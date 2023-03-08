1857dbd06922a397a5d28daf7c3e32ad4b68ddf5
Prachi <prachikale136@gmail.com>
2023-03-08 16:38:57 +0530

    [Prachi] Add: find maximum among three float number

--- FindMaximumNumber/FindMaximumNumber/Program.cs
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -4,13 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+           
             int check = CheckMaxNumber.MaximumNumber(10, 20, 30);
             int check1 = CheckMaxNumber.MaximumNumber(30, 20, 10);
             int check2 = CheckMaxNumber.MaximumNumber(20,30, 10);
             Console.WriteLine(check);
             Console.WriteLine(check1);
             Console.WriteLine(check2);
+            Console.WriteLine("------------------------------");
+
+            //for maximun float value
+            Console.WriteLine("Maximun float number");
+            double number = CheckMaxNumber.MaximunFloatNumber(11.1, 22.2, 33.3);
+            double number1 = CheckMaxNumber.MaximunFloatNumber(33.3, 22.2, 11.1);
+            double number2 = CheckMaxNumber.MaximunFloatNumber(11.1, 33.3, 22.2);
+            Console.WriteLine(number);
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
         }
     }
 }
